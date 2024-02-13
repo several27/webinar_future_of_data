0667da8e4d3590abe953833e05917199f5eff415
several27@icloud.com <several27@icloud.com>
2024-02-13 17:29:20 +0000

    Prophecy basics update

--- pipelines/customers/code/customers/pipeline.py
+++ pipelines/customers/code/customers/pipeline.py
@@ -1,7 +1,6 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from prophecy.utils import *
 from customers.config.ConfigStore import *
 from customers.udfs.UDFs import *
 from prophecy.utils import *
